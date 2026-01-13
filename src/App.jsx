cf2d035648406e600be6c15cda49b9492001221b
MuhammadBek97 <allayevmuhammad8@gmail.com>
2026-01-13 14:54:56 +0500

    creating

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Begzod
+        Ramzbek
+        Shohruh
+        Elyor
+        Botir
+        Eldor
+        Umid
+      </div>
+    </>
+  )
+}
+
+export default App
